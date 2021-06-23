5a8774fa83e1301689781ce9af1a90aed51ad6f4
amandafairuz14 <86370012+amandafairuz14@users.noreply.github.com>
2021-06-24 00:18:17 +0700

    update library
    memasukkan library yang dibutuhkan

--- Final Project_UAS Proglan.c
+++ Final Project_UAS Proglan.c
@@ -1,7 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
+/* Proyek Akhir Pemrograman Lanjut
+Kelas           : Pemrograman Lanjut -02
+Kelompok        : 8
+Nama Anggota    :
+        - Amanda Fairuz Syifa     (2006535653)
+        - Firzy Allen             (2006577555)
+        - Steven Yosua Saputra    (2006577290)
+        - Zana Niswah Awahita     (2006577593)
+Judul Proyek    : Program Perampungan dan Pembuatan SKL (Surat Keterangan Lulus)
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <omp.h>
+
+#define MAX_LENGTH 256
+#define SIZE 100
 
 int main() {
 	return 0;
